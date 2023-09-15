bc79cc94120b4480ec84c041b212aaf4c500fc5a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:37:18 -0300

    adicionando comandos novos

---
+++ aula.js
@@ -0,0 +1,27 @@
+// aqui é um alert
+alert('ola mundo!');
+
+/*
+aprendendo JavaScript.
+Muito legal
+*/
+
+//imprimindo mensagem na tela
+document.write('meu nome é michel')
+
+//usando console
+console.log('Ola, tudo bem?')
+
+//criando nossa primeira variavel
+let mensagem = "Olá, Mundo!"
+console.log('Esse console é da variavel', mensagem);
+
+
+
+
+
+
+
+
+
+

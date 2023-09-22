a046e17c027edf464ec564883918dc52a87d5f6c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 08:51:02 -0300

    adicionando tipos de dados

---
+++ tipos-dados.js
@@ -0,0 +1,17 @@
+// Números inteiros
+let idadeAluno = 18;
+
+//Números flutuantes 
+let valorProduto = 5.35;
+
+//Variavel de texto, string
+let nomeDoAluno = "Michel";
+
+//Booleanos que recebem valor true ou false
+let casado = true;
+
+//Array - conjunto de elementos
+let linguagens = ['java', 'JavaScript', 'c#'];
+
+
+console.log(linguagens[1]);

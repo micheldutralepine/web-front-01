ec00d03afca192d7967d1c3615375a783f2dbf1d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 09:12:29 -0300

    adicionando textos e numeros

---
+++ texto-numeros.js
@@ -0,0 +1,20 @@
+// let numFloat = 123.5422
+
+// let numString = '123.5432'
+
+// //converteu para número
+// console.log(parseFloat(numString))
+// console.log(Number(numString))
+
+
+
+
+
+let numFloat = 123.5422;
+
+let NomeAluno1 = 'Michel';
+
+let text = 'Estao chegando as provas';
+console.log(text.length);
+
+
